5dfd35f0ed7b970d4d8ba6cb189069a278f440e1
manuel <mmonrealgamboa@gmail.com>
2019-12-08 03:24:28 -0600

    a

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ class servidor:
         codigo = client.recv(2)
         print (codigo)
         cod = codigo[0]
-        if int.from_bytes(cod, byteorder='big') != SOLICITAR_CAPTURA: #Si el codigo enviado no fue 10, terminamos la conexion
+        if cod != SOLICITAR_CAPTURA: #Si el codigo enviado no fue 10, terminamos la conexion
             #y mandamos un codigo de error
             return False
 
